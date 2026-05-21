50e748966816f4a0bcfc233e7204fb66feafeafb
clownlessmode <clownlessmode@users.noreply.github.com>
2026-05-21 18:25:45 +0000

    deploy: ee04f9614c1e219916936ed76cccce79b4047e9b

---
+++ _next/static/0doClh_nCgXug0w3UOmi9/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fpayment\u002F[tarrif]","\u002Fpayment\u002F[tarrif]\u002F[payment-method]","\u002Fpayment\u002F[tarrif]\u002Fsuccess"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
